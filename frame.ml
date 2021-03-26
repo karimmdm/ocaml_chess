c4196d135195565846693cc2e9fc6c7e8ef74088
Shaf Ibrahim <smi32@cornell.edu>
2021-03-26 02:03:02 -0700

    Program persistence - will run until user command terminates

--- frame.ml
+++ frame.ml
@@ -1,9 +1,20 @@
 open Graphics
 
+(* [play_game args] is a recursive function that keeps the program running
+  unless the user inputs a "quit" (case-insensitive) command which will
+  terminate the program and close the graphics window. As long as the program
+  is running, the graphics window will still be open.  *)
+let rec play_game args =
+  let input = read_line () in
+  if String.equal (String.lowercase_ascii input) "quit" then
+    ()
+  else 
+    play_game args
+
 let main () =
-  print_endline "Test file";
-  open_graph " 500x500";
+  open_graph " 800x800";
   set_window_title "Chess";
+  play_game "dummy";
   ()
 
 let () = main ()
