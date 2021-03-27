388bcfedcfcc6aa81df385fb82c2d2b13c15202d
Shaf Ibrahim <smi32@cornell.edu>
2021-03-27 01:33:53 -0700

    Specifications for pice, state, and board.

--- main.ml
+++ main.ml
@@ -1,1 +1,21 @@
+open Graphics
 
+(* [play_game args] is a recursive function that keeps the program running.
+  The user can input a "quit" (case-insensitive) command which will
+  terminate the program and close the graphics window. As long as the program
+  is running, the graphics window will still be open. [args] is not used. *)
+let rec play_game args =
+  print_endline "Enter quit to terminate the program: ";
+  let input = read_line () in
+  if String.equal (String.lowercase_ascii input) "quit" then
+    ()
+  else 
+    play_game args
+
+let main () =
+  open_graph " 800x800";
+  set_window_title "Chess";
+  play_game "dummy";
+  ()
+
+let () = main ()
