b7d01bbe5eadf33a4b0ae1875f2423d768aaa553
Shaf Ibrahim <smi32@cornell.edu>
2021-03-28 13:05:28 -0700

    Fixed gui unbound module error

--- gui.mli
+++ gui.mli
@@ -1,24 +1,19 @@
-(* 
-    Module to handle gui changes such as listening for mouse events 
-    and drawing graphics from state. *)
+(* Module to handle gui changes such as listening for mouse events and
+   drawing graphics from state. *)
 
-(* 
-    [draw state] will draw the chess board according to 
-    the state of the board [state] *)
-val draw : State.t -> ()
+(* [draw state] will draw the chess board according to the state of the
+   board [state] *)
+val draw : State.t -> unit
 
 (* [init ()] opens a new window and sets up the gui with size 800x800*)
-val init : unit -> unit 
+val init : unit -> unit
 
 (* [coordinate_pair st] is the coordinate positions (x,y) of the mouse*)
-val coordinate_pair : Graphics.status -> (int * int)
+val coordinate_pair : Graphics.status -> int * int
 
 (* [string_of_coordinate_pair tuple ]*)
-val string_of_coordinate_pair : (int * int) -> String
+val string_of_coordinate_pair : int * int -> string
 
-(* 
-    [listen f] will listen for a mouse click and pass the mouse location to [f]
-    Requires: listen to be executed in a loop 
-    *)
-val listen : ('a -> 'b) -> ()
-
+(* [listen f] will listen for a mouse click and pass the mouse location
+   to [f] Requires: listen to be executed in a loop *)
+val listen : ('a -> 'b) -> unit
