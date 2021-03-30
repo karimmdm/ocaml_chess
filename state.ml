9e899b2a50d46074c3e1bb7ba7637ce4d7f915b2
Shaf Ibrahim <smi32@cornell.edu>
2021-03-30 14:20:59 -0700

    Pawn, bishop refactored

--- state.ml
+++ state.ml
@@ -54,139 +54,202 @@ let is_occupied (grid : Piece.t option list list) (loc : int * int) : bool =
   | Some piece -> true
   | None -> false
 
-(* [piece_move grid loc p] returns true if the given piece can move to
-    the given location in the given grid and false otherwise. *)
-(* let piece_move (grid : Piece.t option list list) (p : Piece.t) 
-  : (int * int) list =
-     *)
-(* let match_piece (piece : Piece.t option) (color : string) : bool =
-  match piece with
-  | Some p -> if String.equal (Piece.color p) color then false else true
-  | None -> false *)
+(* [check_main_diag grid clr x y acc] returns a list of positions along
+  the main diagonal that are empty or a valid capture. *)
+(* let rec check_main_diag grid clr x y acc =
+  if check_bounds grid (x, y) then 
+    let elt = get_elt grid x y in
+      match elt with
+      | Some p -> 
+          if not (String.equal (Piece.color p) clr) then (x, y)::acc
+          else acc
+      | None -> 
+        if String.equal clr "white" then
+          check_main_diag grid clr (x + 1) (y + 1) ((x, y)::acc) @
+          check_main_diag grid clr (x - 1) (y - 1) ((x, y)::acc)
+        else 
+          check_main_diag grid clr (x + 1) (y - 1) ((x, y)::acc) @
+          check_main_diag grid clr (x - 1) (y + 1) ((x, y)::acc)
+  else [] *)
 
-(* [can_capture p st loc] returns a list of all valid positions where the given
-  piece can capture an enemy piece via official chess rules. *)
-let can_capture (p : Piece.t) (st : t) : (int * int) list =
-  let clr = Piece.color p in
-  let x = fst (Piece.position p) in
-  let y = snd (Piece.position p) in
-  (* let locs = [] in *)
-  match Piece.piece_type p with
-  | Pawn ->
-    if String.equal clr "white" then
-      let check_locs = (x + 1, y + 1)::(x - 1, y + 1)::[] in
-      let rec helper lst acc = 
-        match lst with
-        | h::t -> helper t (if check_bounds st.board h then h::acc else acc)
-        | [] -> acc in
-      helper check_locs []
-    else
-      let check_locs = (x + 1, y - 1)::(x - 1, y - 1)::[] in
-      let rec helper lst acc = 
-        match lst with
-        | h::t -> helper t (if check_bounds st.board h then h::acc else acc)
-        | [] -> acc in
-      helper check_locs []
-  (* | Pawn -> 
-    if String.equal clr "white" then
-      if l1 == x + 1 || l1 == x - 1 && l2 == y + 1 &&
-        check_bounds st.board loc then let piece = get_elt st.board x y in
-          match piece with
-          | Some p -> 
-            if String.equal (Piece.color p) "black" then loc::locs else []
-          | None -> []
-      else []
-    else
-      if l1 == x + 1 || l1 == x - 1 && l2 == y - 1 &&
-        check_bounds st.board loc then let piece = get_elt st.board x y in
-          match piece with
-          | Some p ->
-            if String.equal (Piece.color p) "white" then loc::locs else []
-          | None -> []
-      else [] *)
-    (* if ((String.equal clr "white" && (l1 == x + 1 || l1 == x - 1) && l2 == y + 1) 
-      || (l1 == x + 1 || l1 == x - 1) && l2 == y - 1) && 
-        check_bounds st.board loc then
-          match List.nth (List.nth st.board x) y with
-          | Some piece -> 
-            if not (String.equal (Piece.color piece) clr) then loc::locs else []
-          | None -> []
-      else [] *)
-  | Bishop ->
-    if String.equal clr "white" then
-      let rec check_main_diag grid clr x y acc =
-        if check_bounds grid (x, y) then 
-          if String.equal clr "white" then 
-            check_main_diag grid clr (x + 1, y + 1) (x, y)::acc else acc
+(* [check_diag grid clr x y acc diag] returns a list of positions along
+  the diagonal specified by [diag] that are empty or a valid capture. *)
+let rec check_diag grid clr x y acc diag =
+  if check_bounds grid (x, y) then 
+    let elt = get_elt grid x y in
+      match elt with
+      | Some p -> 
+          if not (String.equal (Piece.color p) clr) then (x, y)::acc
+          else acc
+      | None -> 
+        if String.equal clr "white" then
+          if String.equal diag "main" then 
+            check_diag grid clr (x + 1) (y + 1) ((x, y)::acc) diag @
+            check_diag grid clr (x - 1) (y - 1) ((x, y)::acc) diag
+          else
+            check_diag grid clr (x - 1) (y + 1) ((x, y)::acc) diag @
+            check_diag grid clr (x + 1) (y - 1) ((x, y)::acc) diag
+        else 
+          if String.equal diag "main" then 
+            check_diag grid clr (x + 1) (y - 1) ((x, y)::acc) diag @
+            check_diag grid clr (x - 1) (y + 1) ((x, y)::acc) diag
           else 
-            check_main_diag grid clr (x + 1, y - 1) (x, y)::acc else acc in
-      let check_locs = check_diag st.board x y in
-      []
-      (* while check_bounds st.board l do *)
-        (* let piece = get_elt st.board x y in *)
-        (* let rec check_diag loc = *)
-        (* if match_piece piece "white" then loc::locs else []; *)
-        (* match piece with
-        | Some p ->
-          if String.equal (Piece.color p) "white" then loc::locs else []
-        | None -> [] in *)
-        (* l = (x + 1, y + 1) *)
-        (* done; *)
-      (* let l = (x - 1, y - 1) in
-      while check_bounds st.board l do
-        let piece = get_elt st.board x y in
-        if match_piece piece "black" then loc::locs else []; *)
-        (* match piece with
-        | Some p ->
-          if String.equal (Piece.color p) "white" then loc::locs else []
-        | None -> [] in *)
-        (* l = (x - 1, y - 1)
-        done; locs *)
-    else
-      let l = (x - 1, y + 1) in
-      while check_bounds st.board l do
-        let piece = get_elt st.board x y in
-        if match_piece piece "white" then loc::locs else [];
-        (* match piece with
-        | Some p ->
-          if String.equal (Piece.color p) "white" then loc::locs else []
-        | None -> [] in *)
-        l = (x - 1, y + 1)
-        done;
-      let l = (x + 1, y - 1) in
-      while check_bounds st.board l do
-        let piece = get_elt st.board x y in
-        if match_piece piece "black" then loc::locs else [];
-        (* match piece with
-        | Some p ->
-          if String.equal (Piece.color p) "white" then loc::locs else []
-        | None -> [] in *)
-        l = (x + 1, y - 1)
-        done; locs
-  | Knight -> []
-  | Rook -> []
-  | Queen -> []
-  | King -> []
+            check_diag grid clr (x - 1) (y - 1) ((x, y)::acc) diag @
+            check_diag grid clr (x + 1) (y + 1) ((x, y)::acc) diag
+  else acc
+
+(* [check_snd_diag grid clr x y acc] returns a list of positions along
+  the secondary diagonal that are empty or a valid capture. *)
+(* let rec check_snd_diag grid clr x y acc =
+  if check_bounds grid (x, y) then 
+    let elt = get_elt grid x y in
+      match elt with
+      | Some p ->
+        if not (String.equal (Piece.color p) clr) then (x, y)::acc
+        else acc
+      | None ->
+        if String.equal clr "white" then
+          check_snd_diag grid clr (x - 1) (y + 1) ((x, y)::acc) @
+          check_snd_diag grid clr (x + 1) (y - 1) ((x, y)::acc)
+        else 
+          check_snd_diag grid clr (x - 1) (y - 1) ((x, y)::acc) @
+          check_snd_diag grid clr (x + 1) (y + 1) ((x, y)::acc)
+  else [] *)
+
+let rec check_rank board clr x y acc dir = 
+  if check_bounds board (x, y) then
+    let elt = get_elt board x y in
+      match elt with
+      | Some p ->
+        if not (String.equal (Piece.color p) clr) then ((x, y)::acc)
+        else acc
+      | None -> 
+        if String.equal dir "vertical" then
+          check_rank board clr x (y + 1) dir @ 
+          check_rank board clr x (y - 1) dir
+        else
+          check_rank board clr (x + 1) y dir @ 
+          check_rank board clr (x - 1) y dir
+        (* match dir with
+        | "vertical" -> 
+            check_rank board clr x (y + 1) dir
+            (* check_rank board clr x (y + 1) dir @ 
+            check_rank board clr x (y - 1) dir *)
+        | "horizontal" ->
+            (check_rank board clr (x + 1) y dir) @ 
+            (check_rank board clr (x - 1) y dir)
+        | _ -> [] *)
+  else acc
 
 let locations st p =
   let x = fst (Piece.position p) in
   let y = snd (Piece.position p) in
+  let board = st.board in
   let clr = Piece.color p in
-  let width = st.board in
-  match Piece.piece_type p with
-  | Pawn -> 
-      let pawn_move f = 
-        let locs = [] in
-        if 
-      pawn_move ()
+  let piece = Piece.piece_type p in
+  match piece with
+  | Pawn ->
+    (* [pawn_capture board clr x y] returns a list of positions that the given 
+      pawn piece can move to after a legal capture. *)
+    let pawn_capture board clr x y =
+      if String.equal clr "white" then
+        let check_locs = (x + 1, y + 1)::(x - 1, y + 1)::[] in
+        let rec helper lst acc = 
+          match lst with
+          | h::t -> helper t (if check_bounds board h then h::acc else acc)
+          | [] -> acc in
+        helper check_locs []
+      else
+        let check_locs = (x + 1, y - 1)::(x - 1, y - 1)::[] in
+        let rec helper lst acc = 
+          match lst with
+          | h::t -> helper t (if check_bounds board h then h::acc else acc)
+          | [] -> acc in
+        helper check_locs [] in
+    (* [pawn_capture board clr x y] returns a list of positions that the given 
+      pawn piece can move to, excluding captures. *)
+    let pawn_move board clr x y =
+      if String.equal clr "white" then
+        if y == 1 then [(x, y + 1); (x, y + 2)]
+        else if check_bounds board (x, y + 1) then [(x, y + 1)] else []
+      else [] in
+    (pawn_capture board clr x y) @ (pawn_move board clr x y)
   | Bishop ->
-    let bishop_move f = failwith ""
+    (* [bishop_move board clr x y] returns a list of positions that the given
+      bishop piece can move to, including captures. *)
+    let bishop_move board clr x y =
+      (check_diag board clr x y [] "main") @ 
+      (check_diag board clr x y [] "snd") in
+    bishop_move board clr x y
   | Knight ->
-    let knight_move f = failwith ""
-  | Rook -> 
-    let rook_move f = failwith ""
-  | Queen -> failwith ""
-  | King -> failwith ""
+    (* [knight_move board clr x y] returns a list of positions that the given
+      knight piece can move to, including captures. *)
+    let knight_move board clr x y = 
+      (* [valid_positions_helper lst acc] returns [acc] which contains all the  
+        valid board positions in [lst]. *)
+      let rec valid_positions_helper lst acc =
+        match lst with
+        | h::t -> 
+          if check_bounds board h then 
+            let elt = get_elt board x y in
+            match elt with
+            | Some p -> 
+              if not (String.equal (Piece.color p) clr) then 
+                valid_positions_helper t (h::acc)
+              else valid_positions_helper t acc
+            | None -> valid_positions_helper t (h::acc)
+          else valid_positions_helper t acc
+        | [] -> acc in
+      let check_locs = 
+        [(x + 1, y + 2); (x - 1, y + 2); (x + 2, y + 1); (x - 2, y + 1);
+         (x + 1, y - 2); (x - 1, y - 2); (x + 2, y - 1); (x - 2, y - 1)] in
+      valid_positions_helper check_locs [] in 
+    knight_move board clr x y
+  | Rook ->
+    (* [knight_move board clr x y] returns a list of positions that the given
+      knight piece can move to, including captures. *)
+    let rook_move board clr x y = 
+      let rec check_hor_right board clr x y (acc : (int * int) list) = 
+        if check_bounds board (x, y) then
+          let elt = get_elt board x y in
+            match elt with
+            | Some p ->
+              if not (String.equal (Piece.color p) clr) then ((x, y)::acc)
+              else acc
+            | None -> check_hor_right board clr (x + 1) y ((x, y)::acc)
+        else [] in
+      let rec check_hor_left board clr x y (acc : (int * int) list) = 
+        if check_bounds board (x, y) then
+          let elt = get_elt board x y in
+            match elt with
+            | Some p ->
+              if not (String.equal (Piece.color p) clr) then ((x, y)::acc)
+              else acc
+            | None -> check_hor_left board clr (x - 1) y ((x, y)::acc)
+        else [] in
+      let rec check_ver_up board clr x y (acc : (int * int) list) = 
+        if check_bounds board (x, y) then
+          let elt = get_elt board x y in
+            match elt with
+            | Some p ->
+              if not (String.equal (Piece.color p) clr) then ((x, y)::acc)
+              else acc
+            | None -> check_ver_up board clr x (y + 1) ((x, y)::acc)
+        else [] in
+      let rec check_ver_down board clr x y (acc : (int * int) list) = 
+        if check_bounds board (x, y) then
+          let elt = get_elt board x y in
+            match elt with
+            | Some p ->
+              if not (String.equal (Piece.color p) clr) then ((x, y)::acc)
+              else acc
+            | None -> check_ver_down board clr x (y - 1) ((x, y)::acc)
+        else [] in
+      (check_hor_right board clr x y []) @ (check_hor_left board clr x y []) @ 
+      (check_ver_up board clr x y []) @ (check_ver_down board clr x y []) in
+    rook_move board clr x y
+  | Queen -> []
+  | King -> []
 
 let valid_move st (p : Piece.t) loc = 
   (* [check_bounds grid loc] returns true if the given location is within the
