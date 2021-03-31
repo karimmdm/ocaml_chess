ed540e9657ac230274b4994fa57d6b8d03e7e639
Md Karim <55668689+karimmdm@users.noreply.github.com>
2021-03-30 23:27:47 -0400

    Update piece.mli
    remove color variant parameters

--- piece.mli
+++ piece.mli
@@ -20,13 +20,13 @@ type t
    color variant [color],
    icon path [icon] and positon on a 2d array [position]
    requires: [positon] to be (i, j) where i and j are elements of (0,8]) *)
-val make : piece -> color -> string -> int * int -> t
+val make : piece -> string -> string -> int * int -> t
 
 (* [piece_type p] will return a variant of the piece type for piece [p]. *)
 val piece_type : t -> piece
 
 (* [color p] will return a string of the piece's color, white or black. *)
-val color : t -> color
+val color : t -> string
 
 (* [icon p] will return the image file for the current piece. *)
 val icon : t -> string
