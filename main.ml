8080b8de9c54a0397d744c415b60e9337730dc13
Shaf Ibrahim <smi32@cornell.edu>
2021-04-29 00:11:06 -0700

    Deselect implemented

--- main.ml
+++ main.ml
@@ -3,23 +3,18 @@ open Gui
 open State
 open Logic
 
+(* [deselect st] resets the player's turn to the piece selection phase
+   by resetting the current state's piece selected to None. *)
+let deselect st = State.update_piece_clicked st None
+
 (* [piece_selection st pos] validates whether or not [pos] is a valid
    location that contains a piece that the current player can move on
    their turn. *)
 let piece_selection st pos =
   print_endline "Piece selection";
-  (* print_endline
-    ("Piece clicked: "
-    ^ Printer.print_piece_option (State.piece_clicked st)); *)
   let p_turn = State.player_turn st in
   let clr = if p_turn = 1 then "white" else "black" in
   let p = Gui.get_piece st pos in
-  (* print_endline
-    ("Clicked on ("
-    ^ string_of_int (fst pos)
-    ^ ", "
-    ^ string_of_int (snd pos)
-    ^ ")"); *)
   match p with
   | None -> st
   | Some pce ->
@@ -31,27 +26,20 @@ let piece_selection st pos =
    to. *)
 let move_selection st pos =
   print_endline "Move selection";
-  (* print_endline
-    ("Piece clicked: "
-    ^ Printer.print_piece_option (State.piece_clicked st)); *)
   match State.piece_clicked st with
   | None ->
-      failwith "Move selection shouldn't be called when None piece"
+      failwith
+        "Move selection shouldn't be called when piece_selected is None"
   | Some p ->
-      (* print_endline
-        ("Clicked on ("
-        ^ string_of_int (fst pos)
-        ^ ", "
-        ^ string_of_int (snd pos)
-        ^ ")"); *)
       if Logic.valid_move st p pos then Logic.move_piece st p pos
-      else st
+      else deselect st
+(* else st *)
 
 (* [move st pos] checks the current state's piece_selected field to
    determine which phase of the turn the player is in: piece selection
    or move selection. *)
-let move st position =
-  let pos = Gui.invert_pos st position in
+let move st pos =
+  let pos = Gui.invert_pos st pos in
   match State.piece_clicked st with
   | None -> piece_selection st pos
   | Some p -> (
@@ -78,13 +66,9 @@ let play_game st =
       if !current_player = my_player then (
         print_endline ("Player turn " ^ string_of_int !current_player);
         let new_state = Gui.listen (move !current_state) in
-
         game_running := not (State.checkmate new_state);
         current_player := State.player_turn new_state;
         current_state := new_state;
-        (* print_endline ("Piece clicked: " ^ Printer.print_piece_option
-           (State.piece_clicked !current_state)); *)
-        (* print_endline (Printer.print_board !current_state); *)
         Gui.draw !current_state)
       else ()
     done
