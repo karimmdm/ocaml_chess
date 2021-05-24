632fcfddb87cf3ab65a3ebf62290ae455b34e1ea
Shaf Ibrahim <smi32@cornell.edu>
2021-05-24 04:49:28 -0700

    Added ANSI terminal

---
+++ client.ml
@@ -0,0 +1,4 @@
+let make_game () = ANSITerminal.print_string [ ANSITerminal.red ]
+"\n\nWelcome to the 3110 Text Adventure Game engine.\n";
+print_endline
+"Please enter the name of the game file you want to load.\n";
